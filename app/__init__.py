035c8f7a18e8b1334994000f95268a8e825eb1d1
panruopeng <alhprp@gmail.com>
2018-11-02 11:07:51 +0800

    ADD: add get_menu api

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
+import requests
 
 
 app = Flask(__name__)
@@ -8,3 +9,9 @@ app = Flask(__name__)
 @app.route("/index")
 def render_index():
     return render_template("index.html")
+
+
+@app.route("/get_menu")
+def get_menu():
+    r = requests.get("https://www.ele.me/restapi/shopping/v2/menu?restaurant_id=854602&terminal=web")
+    return jsonify(success=1, data=r.json())
