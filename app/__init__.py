8dea412375c31de6a6a89dcb3d3e5b0663e3c31a
panruopeng <alhprp@gmail.com>
2018-11-02 11:03:19 +0800

    ADD: add first page

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,9 @@ from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def render_index():
+    return render_template("index.html")
